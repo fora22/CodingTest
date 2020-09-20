e91a47d81803c4f8403fb33247304d72481d50fb
FoRA <fora22@naver.com>
2020-09-21 00:31:00 +0900

    c++ iteration 공부

--- coding_test/Practice.cpp
+++ coding_test/Practice.cpp
@@ -1,44 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-bool solution(vector<string> phone_book) {
-    bool answer = true;
-    int answerInt;
-    const int phoneBookSize = phone_book.size() - 1;
+int solution(vector<vector<string>> clothes) {
     int i;
+    vector<vector<string>>::iterator iterColumn;
+    vector<string>::iterator iterRaw;
 
-    pair<vector<string>::iterator, vector<string>::iterator> min = minmax_element(phone_book.begin(), phone_book.end());
-    const int phoneBookEraseIndex = find(phone_book.begin(), phone_book.end(), *min.first) - phone_book.begin();
-    vector<string>::iterator phoneBookEraseIterator = phone_book.begin() + phoneBookEraseIndex;
-    string findNumber = *(min.first);
-    phone_book.erase(phoneBookEraseIterator);
-
-    
-    for (i = 0; i < phoneBookSize; i++)
+    for (iterColumn = clothes.begin(); iterColumn != clothes.end(); iterColumn++)
     {
-        answerInt = phone_book[i].find(findNumber, 0);
-        if (answerInt == -1) {
-            continue;
+        for (iterRaw = (*iterColumn).begin(); iterRaw != (*iterColumn).end(); iterRaw++)
+        {
+            cout << *iterRaw << endl;
         }
-        else {
-            answer = false;
-        }
+        
     }
+    
+
+    int answer = 0;
 
     return answer;
 }
 
 int main(void)
 {
-    vector<string> test_1 = { "119", "97674223", "1195524421" };
-    vector<string> test_2= { "123", "456", "789" };
-    vector<string> test_3 = { "12", "123", "1235", "567", "88" };
-    vector<string>* testPtr = &test_1;
-    bool returnAnswer = solution(test_2);
+    vector<vector<string>> test_1 = { {"yellow_hat", "headgear"} ,{"blue_sunglasses", "eyewear"},{"green_turban", "headgear"} };
+    vector<vector<string>> test_2 = {{"crow_mask", "face"}, { "blue_sunglasses", "face" }, { "smoky_makeup", "face" }};
+    int returnAnswer = solution(test_1);
     cout << boolalpha << returnAnswer << endl;
 
     //string s = "41";
