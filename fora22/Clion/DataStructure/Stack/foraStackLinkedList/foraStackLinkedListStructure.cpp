0395dc20dc0b21fa839396e54ea83f7a53c78ad4
fora22 <48875566+fora22@users.noreply.github.com>
2020-10-24 10:59:22 +0900

    폴더 정리_1

--- fora22/Clion/DataStructure/Stack/foraStackLinkedList/foraStackLinkedListStructure.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Assistant_2 on 2020-10-22.
-//
-
-#include "foraStackLinkedListStructure.h"
-
-foraStackLinkedListStructure::foraStackLinkedListStructure() {
-    this->tailNode = NULL;
-}
