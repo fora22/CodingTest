0c31321bbc9eb41886e7a159b90f057adda090c9
fora22 <48875566+fora22@users.noreply.github.com>
2020-10-25 19:17:53 +0900

    3_2번 문제 구현 완료

--- fora22/CodingTest/main.cpp
+++ fora22/CodingTest/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int makeBigNumber(vector<int> numbers, int M, int K) {
+    int firstValue, secondValue;
+    firstValue = numbers[0];
+    secondValue = numbers[1];
+
+    int quotient = M / (K + 1);       // REVIEW - 몫
+    int remainder = M % (K + 1);      // REVIEW - 나머지
+
+    int result = (((firstValue * K) + secondValue) * quotient) + (firstValue * remainder);
+
+    return result;
+}
+
 int main() {
-    int N;  // REVIEW - 거스름 돈
-    cin >> N;
-    vector<int> coins = {500, 100, 50, 10}; // REVIEW - 나눌 코인 종류
-    vector<int> countCoins;
+//    NMK[0]은 N, NMK[1]은 M, NMK[2]는 K
+    vector<int> NMK = {5, 7, 2};        // REVIEW - N, M, K
+    vector<int> N_Number = {3,4,3,4,3};     // REVIEW - N개의 자연수
+    vector<int>::iterator iterNumber;
+    sort(N_Number.begin(), N_Number.end(), greater<int>());
 
-    for (int i = 0; i < coins.size(); i++) {
-        countCoins.push_back(N / coins[i]); // REVIEW - 동전 개수
-        N = N % coins[i];                   // REVIEW - 남은 돈
-    }
-
-    for (int i = 0; i < countCoins.size(); i++) {
-        cout << countCoins[i] << endl;
-    }
+    int result = makeBigNumber(N_Number, NMK[1], NMK[2]);
+    cout << result  << endl;
 
     return 0;
 }
