df04ca5f9e75694fdddee7a17fd9478ec1aa5c53
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2020-11-05 02:27:51 +0900

    3-3 숫자 카드 게임 완료

--- JSY8869/CHAPTER 3/3-3/CodingTest3_3.java
+++ JSY8869/CHAPTER 3/3-3/CodingTest3_3.java
@@ -1,13 +1,12 @@
-import java.util.Random;
+import java.util.*;
 
-class CodingTest3_3{
+public class CodingTest3_3{
     public static void main(String[] args){
-        Test_case mm = new Test_case();
-
-
+        Find_Card_Game Card_Game = new Find_Card_Game();
+        Card_Game.Random_List(); // Test Case 생성
+        Card_Game.Find_Result(); // 결과값 찾기, 출력
     }
 }
-
 
 class Test_case{
     Random random = new Random();
@@ -16,11 +15,22 @@ class Test_case{
     int[][] MK_List = new int[M][K];
 
     void Random_List(){
-        for(int i = 0; i < M; i++){
-            for(int j = 0; i < K; i++){
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < K; j++)
                 MK_List[i][j] = random.nextInt(10000) + 1;
-            }
+        // for(int i = 0; i < M; i++)
+        //     System.out.println(Arrays.toString(MK_List[i])); // Test용
+    }
+}
 
-        }
+class Find_Card_Game extends Test_case{
+    int[] Result = new int[M];
+
+    void Find_Result(){
+        for(int i = 0; i < M; i++){
+            Arrays.sort(MK_List[i]);
+            Result[i] = MK_List[i][0];} // 각 행의 최소값을 저장
+        Arrays.sort(Result);
+        System.out.println(Result[M-1]); // 각 행의 최소값 중 최대값을 출력
     }
 }
