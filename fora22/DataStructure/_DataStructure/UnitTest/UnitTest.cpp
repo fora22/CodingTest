5554de535412473b225ec39294b392c5639d06bf
팽대원 <fora22@kyonggi.ac.kr>
2020-12-01 19:38:02 +0900

    이분 탐색 구현 완료

--- fora22/DataStructure/_DataStructure/UnitTest/UnitTest.cpp
+++ fora22/DataStructure/_DataStructure/UnitTest/UnitTest.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../VS/main.cpp"
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,10 +10,10 @@ namespace UnitTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
+		vector<int> inputData_1{1, 3, 4, 7, 8, 13, 17};
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(0, BinarySearch(0));
+			Assert::AreEqual(5, BinarySearch(inputData_1, inputData_1.size(), 8));
 		}
 	};
 }
