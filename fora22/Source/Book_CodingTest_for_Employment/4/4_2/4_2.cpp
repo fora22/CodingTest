ff6bf692bc9070728547d27f6ebcd1fc01a4fa0d
팽대원 <fora22@kyonggi.ac.kr>
2020-12-02 20:20:07 +0900

    4_2 주석 추가

--- fora22/Source/Book_CodingTest_for_Employment/4/4_2/4_2.cpp
+++ fora22/Source/Book_CodingTest_for_Employment/4/4_2/4_2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#define THREEON 1575
-#define THREEOFF 3600
+/*
+0에서 59 사이에 3이들어가는 수는 다음과 같다.
+
+3, 13, 23, 30 ~ 39, 43, 53 => 15개
+
+따라서 다음과 같이 생각해볼 수 있다.
+
+1. 만약 시간에 3이 없을 경우
+   1. 분에서 3이 있을 경우 - 15 X 초의 모든 경우의 수 60 = 900
+   2. 분에서 3이 없을 경우 - 45 X 초에서 3이 있을 경우의 수 15 = 675
+2. 만약 시간에 3이 있을 경우
+   1. 분, 초 모든 경우의 수 : 60 x 60 = 3600
+*/
+#define THREEON 1575		// 15 x 60 = 900, 45 x 15 = 675 -> 900 + 675 = 1575
+#define THREEOFF 3600		// 60 x 60 = 3600
 
 using namespace std;
 int main(void) {
