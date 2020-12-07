924fc05c701bebf8921f6176de2e2bbcdc089fd3
팽대원 <fora22@kyonggi.ac.kr>
2020-12-07 14:01:08 +0900

    4_3 구현 완료

--- fora22/CodingTest/VS/main.cpp
+++ fora22/CodingTest/VS/main.cpp
@@ -1,61 +1,86 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
-/*
-0에서 59 사이에 3이들어가는 수는 다음과 같다.
-
-3, 13, 23, 30 ~ 39, 43, 53 => 15개
-
-따라서 다음과 같이 생각해볼 수 있다.
-
-1. 만약 시간에 3이 없을 경우
-   1. 분에서 3이 있을 경우 - 15 X 초의 모든 경우의 수 60 = 900
-   2. 분에서 3이 없을 경우 - 45 X 초에서 3이 있을 경우의 수 15 = 675
-2. 만약 시간에 3이 있을 경우
-   1. 분, 초 모든 경우의 수 : 60 x 60 = 3600
-*/
-#define THREEON 1575		// 15 x 60 = 900, 45 x 15 = 675 -> 900 + 675 = 1575
-#define THREEOFF 3600		// 60 x 60 = 3600
+#include <map>
 
 using namespace std;
+
+int solve(string position);
+bool problemInit(vector<vector<int>>* mapPtr, vector<pair<int, int>>* movePtr, map<string, int>* mapColumnPtr);
+int checkNumberOfCases(pair<int, int>* move__, int x, int y);
+
 int main(void) {
-	ifstream readFile("input.txt");
-	string strN;
-	int N;
-	int result = 0;
-	int threecount = 0;
-
-	if (readFile.is_open())
-	{
-		getline(readFile, strN);
-		N = stoi(strN);
-
-		if (N >= 23)
-		{
-			threecount = 3;
-		}
-		else if (N >= 13)
-		{
-			threecount = 2;
-		}
-		else if (N >= 3)
-		{
-			threecount = 1;
-		}
-		else
-		{
-			threecount = 0;
-		}
-
-		result = ((N - threecount + 1) * THREEON) + (threecount * THREEOFF);	// + 1은 0일 때 경우의 수를 추가함
-		cout << result << endl;
-	}
-	else
-	{
-		cout << "파일 열기를 실패했습니다." << endl;
-	}
 	
-	readFile.close();
+	//solve("a1");
 
 	return 0;
+}
+
+int solve(string position) {
+	vector<vector<int>> chessMap(8, vector<int>(8, 0));
+	vector<pair<int, int>> move;
+	map<string,int> mapColumn;
+	vector<pair<int, int>>::iterator iterMove;
+	int numberOfCases = 0;
+
+	if (!problemInit(&chessMap, &move, &mapColumn)) {
+		return -1;
+	}
+	
+	for (iterMove = move.begin(); iterMove != move.end(); iterMove++) {
+		numberOfCases += checkNumberOfCases(&(*iterMove), mapColumn[position.substr(0, 1)], stoi(position.substr(1, 2)));
+	}
+	
+	cout << numberOfCases << endl;
+
+	return numberOfCases;
+}
+
+int checkNumberOfCases(pair<int, int> *move__, int x, int y) {
+	x += move__->first;
+	y += move__->second;
+
+	if ((x > 8) || (y > 8) || (x < 1) || (y < 1)) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+bool problemInit(vector<vector<int>> *mapPtr, vector<pair<int, int>> *movePtr, map<string, int> *mapColumnPtr) {
+	try
+	{
+		// first number of pair is x_axis, second number of pair is y_axis.
+		(*movePtr).push_back(make_pair(2, 1));	// go right and turn left
+		(*movePtr).push_back(make_pair(2, -1));	// go right and turn right
+		(*movePtr).push_back(make_pair(-2, 1));	// go left and turn right
+		(*movePtr).push_back(make_pair(-2, -1));	// go left and turn left
+		(*movePtr).push_back(make_pair(1, 2));	// go up and turn right
+		(*movePtr).push_back(make_pair(1, -2));	// go up and turn left
+		(*movePtr).push_back(make_pair(-1, 2));	// go down and turn left
+		(*movePtr).push_back(make_pair(-1, -2));	// go down and turn right
+
+		for (int i = 0; i < (*mapPtr).size(); i++) {
+			for (int j = 0; j < 8; j++) {
+				(*mapPtr)[i][j] = j + 1;
+			}
+		}
+
+		(*mapColumnPtr).insert(make_pair("a", 1));
+		(*mapColumnPtr).insert(make_pair("b", 2));
+		(*mapColumnPtr).insert(make_pair("c", 3));
+		(*mapColumnPtr).insert(make_pair("d", 4));
+		(*mapColumnPtr).insert(make_pair("e", 5));
+		(*mapColumnPtr).insert(make_pair("f", 6));
+		(*mapColumnPtr).insert(make_pair("g", 7));
+		(*mapColumnPtr).insert(make_pair("h", 8));
+
+		return true;
+	}
+	catch (const std::exception&)
+	{
+		cout << "Init failed" << endl;
+		return false;
+	}
 }
