23b88d92cae43f2a0f7d21e2345c126c7a7c322c
팽대원 <fora22@kyonggi.ac.kr>
2020-12-09 19:40:08 +0900

    4_4 구현 완료

--- fora22/CodingTest/UnitTest/UnitTest.cpp
+++ fora22/CodingTest/UnitTest/UnitTest.cpp
@@ -1,34 +1,38 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../VS/main.cpp"
+#include <fstream>
 
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {
+
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
+		ifstream readfile("input.txt");
+
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(solve("a1"), 2);
+			Assert::AreEqual(solve(&readfile), 3);
 		}
-		TEST_METHOD(TestMethod2)
-		{
-			Assert::AreEqual(solve("h2"), 3);
-		}
-		TEST_METHOD(TestMethod3)
-		{
-			Assert::AreEqual(solve("b7"), 4);
-		}
-		TEST_METHOD(TestMethod4)
-		{
-			Assert::AreEqual(solve("g3"), 6);
-		}
-		TEST_METHOD(TestMethod5)
-		{
-			Assert::AreEqual(solve("e5"), 8);
-		}
+		//TEST_METHOD(TestMethod2)
+		//{
+		//	Assert::AreEqual(solve("h2"), 3);
+		//}
+		//TEST_METHOD(TestMethod3)
+		//{
+		//	Assert::AreEqual(solve("b7"), 4);
+		//}
+		//TEST_METHOD(TestMethod4)
+		//{
+		//	Assert::AreEqual(solve("g3"), 6);
+		//}
+		//TEST_METHOD(TestMethod5)
+		//{
+		//	Assert::AreEqual(solve("e5"), 8);
+		//}
 	};
 }
--- fora22/CodingTest/VS/main.cpp
+++ fora22/CodingTest/VS/main.cpp
@@ -1,79 +1,134 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <string>
-#include <map>
+#include <vector>
 
 using namespace std;
 
-int solve(string position);
-bool problemInit(vector<pair<int, int>> *movePtr, map<string, int> *mapColumnPtr);
-int checkNumberOfCases(pair<int, int>* move__, int x, int y);
+vector<int> initInformation(string str) {
+	int previous = 0;
+	int current = 0;
+	vector<int> information;
+
+	current = str.find(" ");
+	while (current != string::npos)
+	{
+		string substring = str.substr(previous, current - previous);
+		information.push_back(stoi(substring));
+		previous = current + 1;
+		current = str.find(" ", previous);
+	}
+	information.push_back(stoi(str.substr(previous, current - previous)));
+
+	return information;
+}
+
+vector<int>::iterator turnLeft(vector<int>::iterator cD) {
+	if (*cD == 3) {
+		cD--;
+		cD--;
+		cD--;
+	}
+	else {
+		cD++;
+	}
+
+	return cD;
+}
+
+bool confirmMoveableToAll(vector<int> cP, vector<pair<int, int>> m, vector<vector<int>> gM) {
+	int x, y;
+ 	bool checkMoveable = true;
+	for (int i = 0; i < 4; i++) {
+		x = cP[0] + m[i].first;
+		y = cP[1] + m[i].second;
+		checkMoveable = checkMoveable && (gM[x][y] >= 1);
+	}
+
+	return checkMoveable;	// If true character should have to go 4 direction or it is sea.
+}
+
+int solve(ifstream *readFile) {
+	string getInformation;
+	vector<int> mapSize;
+	vector<int> characterPosition;
+	vector<vector<int>> gameMap;
+	vector<int> mapDirection = {0, 1, 2, 3}; // North, East, South, West
+	vector<int>::iterator currentDirection;	// Get Current character Direction
+	int xMap, yMap;
+
+	// Each Index means North, East, South, West. In other words if character move anywhere, you add Value(pair) to your character postion.
+	vector<pair<int, int>> move = { make_pair(-1, 0), make_pair(0, 1), make_pair(1, 0), make_pair(0, -1) };
+
+	// Get Information Init
+	if ((*readFile).is_open()) {
+		getline(*readFile, getInformation);
+		mapSize = initInformation(getInformation);
+		getline(*readFile, getInformation);
+		characterPosition = initInformation(getInformation);
+		for (int i = 0; i < mapSize[0]; i++) {
+			getline(*readFile, getInformation);
+			gameMap.push_back(initInformation(getInformation));
+		}
+	}
+	
+	currentDirection = find(mapDirection.begin(), mapDirection.end(), characterPosition[2]);	
+
+	// Start
+	int count = 0;
+	int debug = 0;
+	while (true) {
+		debug++;
+		if (confirmMoveableToAll(characterPosition, move, gameMap)) {
+			currentDirection = turnLeft(currentDirection);
+			currentDirection = turnLeft(currentDirection);	// If twice turn Left, it is like turn back.
+
+			xMap = characterPosition[0] + move[*currentDirection].first;
+			yMap = characterPosition[1] + move[*currentDirection].second;
+		
+			if (gameMap[xMap][yMap] == 1) {
+				break;
+			}
+			else if (gameMap[xMap][yMap] > 1) {
+				gameMap[xMap][yMap] += 2;
+			}
+			else {
+				gameMap[xMap][yMap] += 2;
+				count++;
+			}
+
+			characterPosition[0] = xMap;
+			characterPosition[1] = yMap;
+
+			currentDirection = turnLeft(currentDirection);
+			currentDirection = turnLeft(currentDirection);	// Turn back for original Direction.
+		}
+		else {
+			currentDirection = turnLeft(currentDirection); // Turn Left
+
+			xMap = characterPosition[0] + move[*currentDirection].first;
+			yMap = characterPosition[1] + move[*currentDirection].second;
+
+			if (gameMap[xMap][yMap] == 0) {
+				gameMap[xMap][yMap] += 2;
+				count++;
+			}
+
+			characterPosition[0] = xMap;
+			characterPosition[1] = yMap;
+		}
+
+	}
+
+	return count;
+}
+
+
 
 int main(void) {
 	
-	//solve("a1");
+	ifstream readfile("input.txt");
+	cout << solve(&readfile) << endl;
 
 	return 0;
-}
-
-int solve(string position) {
-	vector<pair<int, int>> move;
-	map<string,int> mapColumn;
-	vector<pair<int, int>>::iterator iterMove;
-	int numberOfCases = 0;	// result
-
-	if (!problemInit(&move, &mapColumn)) {
-		return -1;		// Initalize
-	}
-	
-	for (iterMove = move.begin(); iterMove != move.end(); iterMove++) {
-		numberOfCases += checkNumberOfCases(&(*iterMove), mapColumn[position.substr(0, 1)], stoi(position.substr(1, 2)));
-	}
-	
-	cout << numberOfCases << endl;
-
-	return numberOfCases;
-}
-
-int checkNumberOfCases(pair<int, int> *move__, int x, int y) {
-	x += move__->first;
-	y += move__->second;
-
-	if ((x > 8) || (y > 8) || (x < 1) || (y < 1)) {
-		return 0;
-	}
-	else {
-		return 1;
-	}
-}
-
-bool problemInit(vector<pair<int, int>> *movePtr, map<string, int> *mapColumnPtr) {
-	try
-	{
-		// first number of pair is x_axis, second number of pair is y_axis.
-		(*movePtr).push_back(make_pair(2, 1));	// go right and turn left
-		(*movePtr).push_back(make_pair(2, -1));	// go right and turn right
-		(*movePtr).push_back(make_pair(-2, 1));	// go left and turn right
-		(*movePtr).push_back(make_pair(-2, -1));	// go left and turn left
-		(*movePtr).push_back(make_pair(1, 2));	// go up and turn right
-		(*movePtr).push_back(make_pair(1, -2));	// go up and turn left
-		(*movePtr).push_back(make_pair(-1, 2));	// go down and turn left
-		(*movePtr).push_back(make_pair(-1, -2));	// go down and turn right
-
-		(*mapColumnPtr).insert(make_pair("a", 1));
-		(*mapColumnPtr).insert(make_pair("b", 2));
-		(*mapColumnPtr).insert(make_pair("c", 3));
-		(*mapColumnPtr).insert(make_pair("d", 4));
-		(*mapColumnPtr).insert(make_pair("e", 5));
-		(*mapColumnPtr).insert(make_pair("f", 6));
-		(*mapColumnPtr).insert(make_pair("g", 7));
-		(*mapColumnPtr).insert(make_pair("h", 8));
-
-		return true;
-	}
-	catch (const std::exception&)
-	{
-		cout << "Init failed" << endl;
-		return false;
-	}
 }
