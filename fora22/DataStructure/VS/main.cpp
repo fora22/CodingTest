3a4ec5ad70b6d4c2d91ce98230a09831b755cd0f
팽대원 <fora22@kyonggi.ac.kr>
2020-12-23 21:00:04 +0900

    random으로 중복 없는 난수 생성  코드 완료

--- fora22/DataStructure/VS/main.cpp
+++ fora22/DataStructure/VS/main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <random>
+#include <vector>
 
 using namespace std;
 
+vector<int> getRandomArray(const int randomNumberCount)
+{
+	int temp, x, y;
+	random_device rd;
+	mt19937 gen(rd());
+
+	uniform_int_distribution<int> dis(0, randomNumberCount - 1);
+
+	vector<int> randomArray(randomNumberCount);
+	
+	for (int i = 0; i < randomNumberCount; i++)
+	{
+		randomArray[i] = i + 1;
+	}
+
+	for (int i = 0; i < randomNumberCount; i++)
+	{
+		x = dis(gen);
+		y = dis(gen);
+		if (x != y)
+		{
+			temp = randomArray[x];
+			randomArray[x] = randomArray[y];
+			randomArray[y] = temp;
+		}
+	}
+
+	return randomArray;
+}
+
+
 int main(void) 
 {
-	ifstream readFile("input.txt");
-	cout << solve(&readFile) << endl;
+	const int randNumberLength = 100;
+	vector<int> randArray = getRandomArray(randNumberLength);
+
+
+
+	
+
+
 	return 0;
 }
