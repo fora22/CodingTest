f29127851aa6a72fc5591f79b039bf41b4690ba1
fora22 <48875566+fora22@users.noreply.github.com>
2021-03-17 15:13:38 +0900

    8_4 Update

--- fora22/CodingTest/VS/main.cpp
+++ fora22/CodingTest/VS/main.cpp
@@ -3,70 +3,28 @@
 #include <string>
 #include <vector>
 
-
 using namespace std;
-
-int BinarySearch(vector<int> inputArray, const int arraySize, const int findData) {
-	int middleIndex;
-	int startIndex = 0;
-	int endIndex = arraySize - 1;
-
-	while (endIndex - startIndex >= 0) {
-		middleIndex = (startIndex + endIndex) / 2;
-		if (inputArray[middleIndex] == findData) {
-			return middleIndex;
-		}
-
-		if (inputArray[middleIndex] < findData) {
-			startIndex = middleIndex + 1;
-		} else {
-			endIndex = middleIndex - 1;
-		}
-	}
-	return -1;
-}
-
-vector<int> sliceArrayComposedInt(string sLine) {
-	int previous = 0;
-	int current = 0;
-	vector<int> resultSliceString;
-
-	current = sLine.find(" ");
-	while (current != string::npos) {
-		string substring = sLine.substr(previous, current - previous);
-		resultSliceString.push_back(stoi(substring));
-		previous = current + 1;
-		current = sLine.find(" ", previous);
-	}
-	resultSliceString.push_back(stoi(sLine.substr(previous, current - previous)));
-	// slice end
-
-	return resultSliceString;
-}
 
 vector<int> solve(ifstream* readFile) {
 	vector<int> result;
-	int N, M;
+	int n;
 	string getInfo;
-	vector<int> allParts, orderParts;
 
 	if ((*readFile).is_open()) {
 		getline(*readFile, getInfo);
-		N = stoi(getInfo);
+		n = stoi(getInfo);
+	}
 
-		getline(*readFile, getInfo);
-		allParts = sliceArrayComposedInt(getInfo);
+	vector<int> dpTable(n);
 
-		getline(*readFile, getInfo);
-		M = stoi(getInfo);
+	dpTable[0] = 1;
+	dpTable[1] = 3;
 
-		getline(*readFile, getInfo);
-		orderParts = sliceArrayComposedInt(getInfo);
+	for (int i = 2; i < dpTable.size(); i++) {
+		dpTable[i] = dpTable[i - 1] + (dpTable[i - 2] * 2);
 	}
 	
-	for (int i = 0; i < orderParts.size(); i++) {
-		result.push_back(BinarySearch(allParts, allParts.size(), orderParts[i]));
-	}
+	result.push_back(dpTable[n - 1] % 796796);
 
 
 	return result;
@@ -77,15 +35,9 @@ int main(void) {
 	ifstream readfile("input.txt");
 	
 	vector<int> solveProblem = solve(&readfile);
-	vector<bool> isPartsHere(solveProblem.size());
+	
 
-	for (int i = 0; i < solveProblem.size(); i++) {
-		if (solveProblem[i] >= 0) {
-			cout << "yes ";
-		} else {
-			cout << "no ";
-		}
-	}
+	cout << solveProblem[0] << endl;
 
 	return 0;
 }
