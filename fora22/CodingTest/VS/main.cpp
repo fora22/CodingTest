fae7dea84c2346ef194d40bb94220679fdad6c03
fora22 <48875566+fora22@users.noreply.github.com>
2021-03-17 16:05:06 +0900

    8_5 complete

--- fora22/CodingTest/VS/main.cpp
+++ fora22/CodingTest/VS/main.cpp
@@ -2,29 +2,72 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int getMin(vector<int> inputArray) {
+	int minValue = *min_element(inputArray.begin(), inputArray.end());
+
+	return minValue;
+}
+
+vector<int> sliceArrayComposedInt(string sLine) {
+	int previous = 0;
+	int current = 0;
+	vector<int> resultSliceInt;
+
+	current = sLine.find(" ");
+	while (current != string::npos) {
+		string substring = sLine.substr(previous, current - previous);
+		resultSliceInt.push_back(stoi(substring));
+		previous = current + 1;
+		current = sLine.find(" ", previous);
+	}
+	resultSliceInt.push_back(stoi(sLine.substr(previous, current - previous)));
+	// slice end
+
+	return resultSliceInt;
+}
+
 vector<int> solve(ifstream* readFile) {
 	vector<int> result;
-	int n;
+	int N, M;
+	vector<int> readInfo;
 	string getInfo;
+	vector<int> moneyCase;
 
 	if ((*readFile).is_open()) {
 		getline(*readFile, getInfo);
-		n = stoi(getInfo);
+		readInfo = sliceArrayComposedInt(getInfo);
+		N = readInfo[0];
+		M = readInfo[1];
+
+		for (int i = 0; i < N; i++) {
+			getline(*readFile, getInfo);
+			moneyCase.push_back(stoi(getInfo));
+		}
 	}
 
-	vector<int> dpTable(n);
+	vector<int> dpTable(M + 1, 10001);
+	vector<int> minVec(2);
+	dpTable[0] = 0;
 
-	dpTable[0] = 1;
-	dpTable[1] = 3;
-
-	for (int i = 2; i < dpTable.size(); i++) {
-		dpTable[i] = dpTable[i - 1] + (dpTable[i - 2] * 2);
+	for (int i = 0; i < moneyCase.size(); i++) {
+		for (int j = moneyCase[i]; j < (M + 1); j++) {
+			if ((dpTable[j - moneyCase[i]]) != 10001) {
+				minVec[0] = dpTable[j];
+				minVec[1] = dpTable[j - moneyCase[i]] + 1;
+				dpTable[j] = getMin(minVec);
+			}
+		}
 	}
-	
-	result.push_back(dpTable[n - 1] % 796796);
+
+	if (dpTable[M] != 10001) {
+		result.push_back(dpTable[M]);
+	} else {
+		result.push_back(-1);
+	}
 
 
 	return result;
@@ -33,9 +76,9 @@ vector<int> solve(ifstream* readFile) {
 int main(void) {
 
 	ifstream readfile("input.txt");
-	
+
 	vector<int> solveProblem = solve(&readfile);
-	
+
 
 	cout << solveProblem[0] << endl;
 
