e75ed1e7fdae3e516a54d2038676000b25903a7b
JSY8869 <65009713+JSY8869@users.noreply.github.com>
2021-06-24 15:03:23 +0900

    11_4 수정 완료
    11_4 수정 완료

--- JSY8869/CHAPTER 11/실전 11_4/CodingTest11_4.java
+++ JSY8869/CHAPTER 11/실전 11_4/CodingTest11_4.java
@@ -1,17 +1,15 @@
 import java.util.*;
 class CodingTest11_4{
     public static void main(String[] args){
-        int N = 5;
+        int N = 4;
         int i = 1;
-        int[] coin_value_list = {3, 2, 1, 1, 9};
+        int[] coin_value_list = {1,2,3,6};
         Arrays.sort(coin_value_list);
-        loop : while(true){
-            for(int x : coin_value_list){
-                if (i < x){
-                    break loop;
-                }
-                i += x;
+        for(int x : coin_value_list){
+            if (i < x){
+                break;
             }
+            i += x;
         }
         System.out.println(i);
     }
