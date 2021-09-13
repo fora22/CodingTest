06ee0a2755f34e9e0bb9c9c7cd482c9952d860e9
FoRA <48875566+fora22@users.noreply.github.com>
2021-09-13 17:47:47 +0900

    26번 완료

--- fora22/CodingTest/python/main.py
+++ fora22/CodingTest/python/main.py
@@ -3,18 +3,24 @@ import sys
 dirName = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
 
-stages = list(map(int, input().split()))
-stages.sort()
-print(stages)
+N = int(input())
+cards = []
+for i in range(N):
+    cards.append(int(input()))
+print(cards)
+
+count = cards[0]
+
+lenCards = len(cards)
+
+for i in range(1, lenCards):
+    if i != (lenCards-1):
+        count += cards[i]
+        count *= 2
+    else:
+        count += cards[i]
+
+print(count)
 
 
-import copy
-testStages = copy.deepcopy(stages)
-
-for st in range(1, max(stages) + 1):
-    countStages = testStages.count(st)
-    lenS = len(testStages)
-    fail = countStages / lenS
-    testStages.remove(st)
-    print(testStages)    
 
