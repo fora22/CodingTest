dbe47098834c03c82fc5336798ca5c99919b402e
FoRA <48875566+fora22@users.noreply.github.com>
2021-09-13 10:22:10 +0900

    25번 푸는중

--- fora22/CodingTest/python/main.py
+++ fora22/CodingTest/python/main.py
@@ -3,29 +3,18 @@ import sys
 dirName = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
 
-N = int(input())
-numbers = list(map(int, input().split()))
-operations = list(map(int, input().split()))  # plus, minus, multiply, divide
+stages = list(map(int, input().split()))
+stages.sort()
+print(stages)
 
-maxNum = -10**9
-minNum = 10**9
 
-def dfs(num, depth, pl, mi, mu, di):
-    global maxNum, minNum
-    if depth == N:
-        maxNum = max(num, maxNum)
-        minNum = min(num, minNum)
-        return
+import copy
+testStages = copy.deepcopy(stages)
 
-    if pl > 0:
-        dfs(num + numbers[depth], depth + 1, pl - 1, mi, mu, di)
-    if mi > 0:
-        dfs(num - numbers[depth], depth + 1, pl, mi - 1, mu, di)
-    if mu > 0:
-        dfs(num * numbers[depth], depth + 1, pl, mi, mu - 1, di)
-    if di > 0:
-        dfs(num / numbers[depth], depth + 1, pl, mi, mu, di - 1)
+for st in range(1, max(stages) + 1):
+    countStages = testStages.count(st)
+    lenS = len(testStages)
+    fail = countStages / lenS
+    testStages.remove(st)
+    print(testStages)    
 
-dfs(numbers[0], 1, operations[0], operations[1], operations[2], operations[3])
-print(maxNum)
-print(minNum)
