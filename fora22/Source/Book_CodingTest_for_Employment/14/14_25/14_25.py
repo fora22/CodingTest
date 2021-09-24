fe4015f651e543626a0d8072844debc0acda912f
FoRA <48875566+fora22@users.noreply.github.com>
2021-09-24 09:02:23 +0900

    25, 26번 PR

---
+++ fora22/Source/Book_CodingTest_for_Employment/14/14_25/14_25.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+dirName = os.path.dirname(os.path.abspath(__file__))
+sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
+
+N = int(input())
+stages = list(map(int, input().split()))
+stages.sort()
+print(stages)
+
+
+import copy
+testStages = copy.deepcopy(stages)
+
+for st in range(1, max(stages) + 1):
+    countStages = testStages.count(st)
+    lenS = len(testStages)
+    fail = countStages / lenS
+    testStages.remove(st)
+    print(testStages)    
---
+++ fora22/Source/Book_CodingTest_for_Employment/14/14_26/14_26.py
@@ -0,0 +1,23 @@
+import os
+import sys
+dirName = os.path.dirname(os.path.abspath(__file__))
+sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
+
+N = int(input())
+cards = []
+for i in range(N):
+    cards.append(int(input()))
+print(cards)
+
+count = cards[0]
+
+lenCards = len(cards)
+
+for i in range(1, lenCards):
+    if i != (lenCards-1):
+        count += cards[i]
+        count *= 2
+    else:
+        count += cards[i]
+
+print(count)
