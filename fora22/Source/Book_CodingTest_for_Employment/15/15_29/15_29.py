af290fdabedb5dc6e9d347c5cbb3228b656f0246
FoRA <fora22@naver.com>
2021-10-15 17:30:41 +0900

    15_29 푸는중

--- fora22/Source/Book_CodingTest_for_Employment/15/15_29/15_29.py
+++ fora22/Source/Book_CodingTest_for_Employment/15/15_29/15_29.py
@@ -3,14 +3,7 @@ import sys
 dirName = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
 
-
-NC = list(map(int, sys.stdin.readline().rstrip().split()))
-N = NC[0]
-C = NC[1]
-
-xp = []
-for i in range(N):
-    xp.append(int(input()))
-
+N,C = map(int, sys.stdin.readline().split())
+xp = [int(sys.stdin.readline()) for _ in range(N)]
 
 xp.sort()
