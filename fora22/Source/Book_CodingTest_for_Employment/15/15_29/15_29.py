ae0d2ee11011e1a31a27ea9cc346ca2885fbb6c5
FoRA <fora22@naver.com>
2021-10-18 09:52:28 +0900

    29번 문제 해결

--- fora22/Source/Book_CodingTest_for_Employment/15/15_29/15_29.py
+++ fora22/Source/Book_CodingTest_for_Employment/15/15_29/15_29.py
@@ -4,6 +4,29 @@ dirName = os.path.dirname(os.path.abspath(__file__))
 sys.stdin = open(os.path.abspath(dirName + "/input.txt"), 'r')
 
 N,C = map(int, sys.stdin.readline().split())
-xp = [int(sys.stdin.readline()) for _ in range(N)]
+array = [int(sys.stdin.readline()) for _ in range(N)]
 
-xp.sort()
+array.sort()
+
+start = 1
+end = array[-1] - array[0]
+
+maxLength = 0
+
+while (start <= end):
+    mid = (start + end) // 2
+    install = array[0]
+    count = 1
+
+    for x in array:
+        if (x >= install + mid):
+            install = x
+            count = count + 1
+    
+    if count >= C:
+        maxLength = max([maxLength, mid])
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(maxLength)
