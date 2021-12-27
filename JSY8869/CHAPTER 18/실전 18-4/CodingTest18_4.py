8a12bfa9ae22371fafdea47bea7e5a069b0611f4
Jeong SeYoung <65009713+JSY8869@users.noreply.github.com>
2021-12-27 22:15:14 +0900

    주석 추가

--- JSY8869/CHAPTER 18/실전 18-4/CodingTest18_4.py
+++ JSY8869/CHAPTER 18/실전 18-4/CodingTest18_4.py
@@ -1,7 +1,7 @@
 # 특정 원소가 속한 집합을 찾기
 def find_parent(parent, x):
     # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
-    if parent[x] != x:
+    if parent[x] != x: # 부모가 자기 자신인 경우를 찾을때까지
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
@@ -34,7 +34,7 @@ for i in range(v):
     y.append([b,i])
     z.append([c,i])
 
-# 간선을 비용순으로 정렬
+# 좌표 오름차순 정렬
 x.sort()
 y.sort()
 z.sort()
@@ -44,14 +44,14 @@ for i in range(v-1):
   tunnels.append([abs(x[i+1][0] - x[i][0]), x[i][1], x[i+1][1]])
   tunnels.append([abs(y[i+1][0] - y[i][0]), y[i][1], y[i+1][1]])
   tunnels.append([abs(z[i+1][0] - z[i][0]), z[i][1], z[i+1][1]])
-tunnels.sort()
 
+tunnels.sort()
 
 for tunnel in tunnels:
     cost, a, b = tunnel
 
     if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
+        union_parent(parent, a, b) # 공통된 부모를 가지도록
         result += cost
-
+        
 print(result)
