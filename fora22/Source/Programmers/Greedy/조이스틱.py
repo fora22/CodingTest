a52b64fabcaf4fd0a6e5579e49e03760b37dd427
FoRA <fora22@naver.com>
2022-01-12 13:12:18 +0900

    Greedy 조이스틱 완료 220112

--- fora22/Source/Programmers/Greedy/조이스틱.py
+++ fora22/Source/Programmers/Greedy/조이스틱.py
@@ -1,7 +1,34 @@
+'''
+Greedy
+참고
+https://dev-note-97.tistory.com/96
+'''
 def solution(name):
     name_l = list(name)
-    temp = [min(abs(x - ord("A")), abs(x - ord("Z"))) for x in list(map(ord,name_l))]
-    answer = sum(temp) + len(temp) - 1
+    moves = [min(abs(x - ord("A")), abs(x - ord("Z")) + 1) for x in list(map(ord,name_l))]
+    answer = sum(moves)
+
+    idx = 0
+    while True:
+        moves[idx] = 0
+        if sum(moves) == 0:
+            break
+        
+        left, right = 1, 1
+
+        while moves[idx - left] == 0:
+            left += 1
+        
+        while moves[idx + right] == 0:
+            right += 1
+        
+        if left >= right:
+            idx += right
+            answer += right
+        else:
+            idx -= left
+            answer += left
+
     return answer
 
 n = ["JEROEN", "JAN", "JAZ"]
