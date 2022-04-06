0266c60d61b7ebaef66df005d04e43d785fa1a10
FoRA <48875566+fora22@users.noreply.github.com>
2022-04-06 15:31:08 +0900

    단속카메라 완료

--- fora22/Source/Programmers/Greedy/단속카메라.py
+++ fora22/Source/Programmers/Greedy/단속카메라.py
@@ -1,33 +1,18 @@
 '''
 Greedy
-
+참고
+https://wwlee94.github.io/category/algorithm/greedy/speed-enforcement-camera/
 '''
-from collections import deque
-
 def solution(routes):
-    routes.sort()
-    _in, _out = -3000, 3000
+    routes.sort(key=lambda x: x[1])
+    camera = -30001
     answer = 0
 
-    queue = deque(routes)
-    while queue:
-        cctvRange = list(range(_in, _out + 1))
-        route = queue.popleft()
-        if route[0] not in cctvRange:
-            answer += 1
-            _in, _out = route[0], route[1]
-        else:
-            if _in < route[0]:
-                _in = route[0]
-            if _out > route[1]:
-                _out = route[1]
+    for route in routes:
+        if camera < route[0]:
+            answer += 1 
+            camera = route[1]
 
-    answer += 1        
-            
-        
-
-        
-        
     return answer
 
 r = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
